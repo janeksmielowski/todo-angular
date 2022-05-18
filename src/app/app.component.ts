0d2b76b927104a9e8b53dddcedb2ac0e3dbaed49
janeksmielowski <janek.smielowski@gmail.com>
2022-05-18 21:52:08 +0200

    Todo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+interface TodoItem {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('newTodoInput') newTodoInput!: ElementRef;
+
   title = 'todo-angular';
+  todoItems: TodoItem[] = [
+    {
+      id: 0,
+      title: 'Do the shopping',
+      completed: false
+    }, {
+      id: 1,
+      title: 'Go to the gym',
+      completed: false
+    }, {
+      id: 2,
+      title: 'Do the laundry',
+      completed: false
+    }
+  ];
+  nextTodoId = 3;
+  get todoItemsIncomplete() {
+    return this.todoItems.filter(item => !item.completed);
+  }
+  get todoItemsComplete() {
+    return this.todoItems.filter(item => item.completed);
+  }
+  addTodo(newTodo: string) {
+    if (newTodo) {
+      const newItem: TodoItem = {
+        id: this.nextTodoId++,
+        title: newTodo,
+        completed: false
+      };
+      this.todoItems.push(newItem);
+      this.newTodoInput.nativeElement.value = '';
+    }
+  }
+  removeTodo(item: TodoItem) {
+    const index = this.todoItems.indexOf(item);
+    this.todoItems.splice(index, 1);
+  }
+  toggleTodo(item: TodoItem) {
+    item.completed = !item.completed;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,34 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
